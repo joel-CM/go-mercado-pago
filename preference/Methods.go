3ab32a2423ce128ef664ca9528b8d7384abe9a89
joel-cM <joelchavezm19@gmail.com>
2023-03-07 21:33:57 -0300

    metodos del tipo preference

---
+++ preference/Methods.go
@@ -0,0 +1,46 @@
+package preference
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	mercadopago "github.com/joel-CM/go_mercadopago"
+)
+
+func (p Preference) Create() (ResponsePreference, error) {
+	var client *http.Client = &http.Client{}
+	var respPref ResponsePreference = ResponsePreference{}
+
+	jsonPreference, jsonPreferenceErr := json.Marshal(p)
+	if jsonPreferenceErr != nil {
+		return respPref, jsonPreferenceErr
+	}
+
+	req, reqErr := http.NewRequest(
+		"POST",
+		mercadopago.ApiPreferenceMP,
+		bytes.NewBuffer(jsonPreference),
+	)
+	if reqErr != nil {
+		return respPref, reqErr
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %v", mercadopago.AccessToken))
+
+	resp, respErr := client.Do(req)
+	if respErr != nil {
+		return respPref, respErr
+	}
+
+	defer resp.Body.Close()
+
+	decodeErr := json.NewDecoder(resp.Body).Decode(&respPref)
+	if decodeErr != nil {
+		return respPref, decodeErr
+	}
+
+	return respPref, nil
+}
