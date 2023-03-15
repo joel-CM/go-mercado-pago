afd0e620587e2abaa0cf2e7f359bafc55d76c484
joel-cM <joelchavezm19@gmail.com>
2023-03-14 21:57:29 -0300

    cambie el type de live_mode a string

--- notification/NotificationType.go
+++ notification/NotificationType.go
@@ -2,7 +2,7 @@ package notification
 
 type Notification struct {
 	ID          string `json:"id"`
-	LiveMode    bool   `json:"live_mode"`
+	LiveMode    string `json:"live_mode"`
 	Type        string `json:"type"`
 	DateCreated string `json:"date_created"`
 	UserId      int    `json:"user_id"`
