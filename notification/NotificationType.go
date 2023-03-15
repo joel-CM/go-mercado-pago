58a5f76d93085f17099a011c9b56a07583b1ab6d
joel-cM <joelchavezm19@gmail.com>
2023-03-15 17:49:32 -0300

    cambie el tipo user_id notification

--- notification/NotificationType.go
+++ notification/NotificationType.go
@@ -5,7 +5,7 @@ type Notification struct {
 	LiveMode    bool   `json:"live_mode"`
 	Type        string `json:"type"`
 	DateCreated string `json:"date_created"`
-	UserId      int    `json:"user_id"`
+	UserId      string    `json:"user_id"`
 	ApiVersion  string `json:"api_version"`
 	Action      string `json:"action"`
 	Data        struct {
