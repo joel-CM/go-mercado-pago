896bd01dd1e64f16c8b33c83cbde33ed62732932
joel-cM <joelchavezm19@gmail.com>
2023-03-16 11:41:03 -0300

    agregue el tipo PaymentMethods

--- preference/PreferenceType.go
+++ preference/PreferenceType.go
@@ -5,4 +5,8 @@ type Preference struct {
 	BackUrls        BackUrls        `json:"back_urls"`
 	NotificationUrl string          `json:"notification_url"`
 	Payer           PayerPreference `json:"payer"`
+	PaymentMethods  struct {
+		Installments        int `json:"installments"`
+		DefaultInstallments int `json:"default_installments"`
+	} `json:"payment_methods"`
 }
