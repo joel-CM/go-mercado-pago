b3f9c4dbc3ebae983cce24f539f483612b8c1ab5
joel-cM <joelchavezm19@gmail.com>
2023-03-16 11:47:54 -0300

    agregue la variable global ApiPaymentMP de tipo string

--- SetUp.go
+++ SetUp.go
@@ -3,9 +3,11 @@ package mercadopago
 var AccessToken string
 var ApiPreferenceMP string
 var ApiOrderMP string
+var ApiPaymentMP string
 
-func SetUp(access_token string, api_preference_mp string, api_order_mp string) {
+func SetUp(access_token string, api_preference_mp string, api_order_mp string, api_payment_mp string) {
 	AccessToken = access_token
 	ApiPreferenceMP = api_preference_mp
 	ApiOrderMP = api_order_mp
+	ApiPaymentMP = api_payment_mp
 }
